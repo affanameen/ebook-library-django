99b974a8819b31381237232aa65a815783e2b74e
affanameen <affanameen14@hotmail.com>
2025-10-29 01:15:21 -0400

    minor updates

--- mysiteF25/urls.py
+++ mysiteF25/urls.py
@@ -4,9 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # OLD (Lab 5):
-    # path('myapp/', include('myapp.urls1')),
-
-    # NEW (Lab 6):
     path('myapp/', include('myapp.urls')),
 ]
